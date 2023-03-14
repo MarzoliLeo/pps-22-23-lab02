3536c7ecf550e4f41aba9ef9b3a69a93a63600e9
Leo_Marzoli <leo.marzoli@studio.unibo.it>
2023-03-14 22:35:17 +0100

    Task3 Recursion implementato.

--- src/u02/Task2/Task3.scala
+++ src/u02/Task2/Task3.scala
@@ -1,5 +1,18 @@
 package u02.Task2
 
-object Task3 {
+object Task3 extends App{
+  def gcd(a: Int, b: Int): Int = b match
+    case 0 => a
+    case _ => gcd(b, a % b )
 
+  def gcdTail(a: Int, b: Int): Int =
+    @annotation.tailrec
+    def loop(x: Int, y: Int): Int = y match
+        case 0 => x
+        case _ => loop(y, x % y)
+    loop(a,b)
+
+
+  println(gcd(36,24)) // 12
+  println(gcdTail(36,24)) // 12
 }
